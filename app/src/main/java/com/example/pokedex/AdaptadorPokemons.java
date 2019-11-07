46db3b219277160f2517e381febe59ca6ebccf20
Jordi Joan <jordi.joan@mataro.epiaedu.cat>
2019-11-07 12:00:19 +0100

    primer intent d'omplenar el list view

--- app/src/main/java/com/example/pokedex/AdaptadorPokemons.java
+++ app/src/main/java/com/example/pokedex/AdaptadorPokemons.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 class AdaptadorPokemons extends ArrayAdapter<Pokemon> {
@@ -17,9 +18,22 @@ class AdaptadorPokemons extends ArrayAdapter<Pokemon> {
         LayoutInflater inflater = LayoutInflater.from(getContext());
         View item = inflater.inflate(R.layout.layout_pokemon, null);
 
-        Pokemon pokemon = (Pokemon) getItem(position);
+        Pokemon pokemon = getItem(position);
 
+        ImageView ivIcon = item.findViewById(R.id.ivIcon);
+        ivIcon.setImageResource(R.drawable.ic_bulbasaur);
 
+        TextView tvNom = item.findViewById(R.id.tvNom);
+        tvNom.setText(pokemon.getNom());
+
+        TextView tvTipo1 = item.findViewById(R.id.tvTipo1);
+        tvNom.setText(pokemon.getNom());
+
+        TextView tvTipo2 = item.findViewById(R.id.tvTipo2);
+        tvNom.setText(pokemon.getNom());
+
+        TextView tvtvHabilitat = item.findViewById(R.id.tvHabilitat);
+        tvNom.setText(pokemon.getNom());
 
         return(item);
     }
