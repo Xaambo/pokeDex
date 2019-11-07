241173ae56b4e49d2f37f3a9d5d8378fc74021ac
Jordi Joan <jordi.joan@mataro.epiaedu.cat>
2019-11-07 11:47:24 +0100

    creació list view

--- app/src/main/java/com/example/pokedex/MainActivity.java
+++ app/src/main/java/com/example/pokedex/MainActivity.java
@@ -3,23 +3,28 @@ package com.example.pokedex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     private AdaptadorPokemons adaptador;
 
-    private Pokemon[] Pokemons =
-            new Pokemon[]{
-                    new Pokemon("Bulbasaur", "Espesura", "Planta", "Veneno", "No tiene", "Ivysaur"),
-                    new cAmigo("amigo 02", "Apellidos 02", "675998802", "hola02@02.com"),
-                    new cAmigo("amigo 03", "Apellidos 03", "675998803", "hola03@03.com"),
-                    new cAmigo("amigo 04", "Apellidos 04", "675998804", "hola04@04.com")
+    private Pokemon[] Pokemons = new Pokemon[]{
+                                 new Pokemon("Bulbasaur", "Espesura", "Planta", "Veneno", "No tiene", "Ivysaur"),
+                                 new Pokemon("Inysaur", "Espesura", "Planta", "Veneno", "Bulbasaur", "Venusaur"),
+                                 new Pokemon("Venusaur", "Espesura", "Planta", "Veneno", "Inysaur", "No tiene"),
+                                 new Pokemon("Charmander", "Mar llamas", "Fuego", "No tiene", "No tiene", "Charmeleon")
+                                 };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AdaptadorPokemons adaptador = new AdaptadorPokemons(this, Pokemons);
 
+        // Assignem al listview l'adaptador creat
+        ListView lst = (ListView)findViewById(R.id.lvPokemon);
+        lst.setAdapter(adaptador);
     }
 }
