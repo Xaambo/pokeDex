439a706dbfe3b8e6fd2f44c5767af838642c865f
Jordi Joan <jordi.joan@mataro.epiaedu.cat>
2019-11-24 20:48:26 +0100

    llogica dels noms als botons fets

--- app/src/main/java/com/example/pokedex/Game.java
+++ app/src/main/java/com/example/pokedex/Game.java
@@ -2,6 +2,8 @@ package com.example.pokedex;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,12 +21,74 @@ public class Game extends AppCompatActivity {
         final ArrayList<Pokemon> pokemons = (ArrayList<Pokemon>) getIntent().getSerializableExtra("pokemons");
 
         int random = (int) (Math.random() * (pokemons.size()));
+        int foto;
+        String nom;
+
+        ArrayList<Button> opcions = new ArrayList<>();
+
+        Button btnOpcio1 = findViewById(R.id.btnOpcio1);
+        Button btnOpcio2 = findViewById(R.id.btnOpcio2);
+        Button btnOpcio3 = findViewById(R.id.btnOpcio3);
+        Button btnOpcio4 = findViewById(R.id.btnOpcio4);
+
+        opcions.add(btnOpcio1);
+        opcions.add(btnOpcio2);
+        opcions.add(btnOpcio3);
+        opcions.add(btnOpcio4);
 
         Pokemon pokemon = pokemons.get(random);
+        Pokemon pokemonFalso;
+
+        nom = pokemon.getNom();
+        foto = pokemon.getFoto();
 
         ImageView ivPokemon = findViewById(R.id.ivGPokemon);
-        ivPokemon.setImageResource(pokemon.getFoto());
+        ivPokemon.setImageResource(foto);
         ivPokemon.setColorFilter(ContextCompat.getColor(this, R.color.black), android.graphics.PorterDuff.Mode.MULTIPLY);
+
+        random = (int) (Math.random() * opcions.size());
+
+        opcions.get(random).setText(nom);
+
+        opcions.remove(random);
+
+        for (int i = 0; i < opcions.size(); i++) {
+            random = (int) (Math.random() * (pokemons.size()));
+            pokemonFalso = pokemons.get(random);
+
+            if (foto != pokemonFalso.getFoto()) {
+                opcions.get(i).setText(pokemons.get(random).getNom());
+            }
+        }
+
+        btnOpcio1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnOpcio2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnOpcio3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnOpcio4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
 
     }
 }
